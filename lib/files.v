b569a08ea23d58c4f5c9f14e31864d39990f8b2c
Dmitry Ponyatov <dponyatov@gmail.com>
2026-06-19 12:09:36 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ lib/files.v
@@ -0,0 +1,20 @@
+mkdir .vscode
+mkdir  vscode
+mkdir bin
+mkdir doc
+mkdir inc
+mkdir lib
+mkdir ref
+mkdir src
+mkdir tmp
+
+echo '!.gitignore' > .vscode/.gitignore
+echo '!.gitignore' >  vscode/.gitignore
+echo '!.gitignore' > bin/.gitignore
+echo '!.gitignore' > doc/.gitignore
+echo '!.gitignore' > inc/.gitignore
+echo '!.gitignore' > lib/.gitignore
+echo '!.gitignore' > ref/.gitignore
+echo '!.gitignore' > src/.gitignore
+echo '!.gitignore' > tmp/.gitignore
+echo '!.gitignore' >   ./.gitignore
